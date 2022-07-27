64f7c94562c30cdff15e3c00aa05ad33e2c26380
Torres-ssf <sergio.uft@gmail.com>
2022-07-27 15:10:58 -0300

    implementing app entry file

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,27 @@
-console.log('Hello World')
+import { fetchInParallel } from './lib/fetchInParallel'
+import { countSuccessfulRequests, urls } from './utils'
+
+
+
+const concorrent = Number(process.argv[2]) || 5
+const timeout = 5000
+
+const fetchUrls = async (urls: string[], concorrent: number) => {
+  try {
+    const result = await fetchInParallel({
+      urls,
+      concorrent,
+      timeout
+    })
+
+    const num = countSuccessfulRequests(result)
+
+    const msg = `Successfully fetched ${num} urls (max ${concorrent} concurrently)`
+
+    console.info(msg)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+fetchUrls(urls, concorrent)
